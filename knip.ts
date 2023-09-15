b7464689ac7a83e098d5f22ee1630ff10b34d303
Thomas B Homburg <thomas@homburg.dk>
2023-09-15 11:14:54 +0200

    Base config

--- knip.ts
+++ knip.ts
@@ -1,15 +1,5 @@
 import type { KnipConfig } from "knip";
 
-const config: KnipConfig = {
-  entry: ["*.vitest.ts", "**/*.vitest.ts"],
-  workspaces: {
-    "*": {
-      entry: "**/*.vitest.ts",
-    },
-    "*/*": {
-      entry: "**/*.vitest.ts",
-    },
-  },
-};
+const config: KnipConfig = {};
 
 export default config;
