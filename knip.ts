f9d0ddf7b77b4a7cb5d110632891883f4e10209d
Thomas B Homburg <thomas@homburg.dk>
2023-09-15 09:53:16 +0200

    More setup

--- knip.ts
+++ knip.ts
@@ -1,7 +1,15 @@
-import type { KnipConfig } from 'knip';
+import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
-  entry: ['*.vitest.ts', '**/*.vitest.ts'],
+  entry: ["*.vitest.ts", "**/*.vitest.ts"],
+  workspaces: {
+    "*": {
+      entry: "**/*.vitest.ts",
+    },
+    "*/*": {
+      entry: "**/*.vitest.ts",
+    },
+  },
 };
 
 export default config;
