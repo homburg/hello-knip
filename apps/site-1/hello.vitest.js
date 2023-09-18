a5c6e647f0b05afb3f1a9038cfdc97fd13bc1c1f
Thomas B Homburg <thomas@homburg.dk>
2023-09-18 12:45:10 +0200

    Next site lib import

---
+++ apps/site-1/hello.vitest.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const vitest_1 = require("vitest");
+(0, vitest_1.test)("adds 1 + 2 to equal 3", () => {
+    (0, vitest_1.expect)(3).toBe(3);
+});
