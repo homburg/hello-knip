84b6c3e7b3098458f0bd9b1666096dc404ad433c
Thomas B Homburg <thomas@homburg.dk>
2023-10-24 10:41:50 +0200

    test (#16)

--- apps/next-site/next.config.js
+++ apps/next-site/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["route.tsx", "route"],
+  transpilePackages: ["@hello-knip/lib-1"],
 };
 
 module.exports = nextConfig;
